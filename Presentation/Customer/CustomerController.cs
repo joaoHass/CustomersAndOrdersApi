bcd65d88b461eb2c391729a361ef93c40a09aa04
joao.hass <joaopedrohass@outlook.com>
2023-07-16 22:46:36 -0300

    fix change entities reference path on Controllers

--- Presentation/Customer/CustomerController.cs
+++ Presentation/Customer/CustomerController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Domain.Customer;
+using Domain.Customers;
 
 namespace Presentation
 {
--- Presentation/Order/OrderController.cs
+++ Presentation/Order/OrderController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Domain.Order;
+using Domain.Orders;
 
 namespace Presentation
 {
