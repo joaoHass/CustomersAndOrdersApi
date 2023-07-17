ff8eb796808f543343bd881ff47ec371ce6e2f4c
joao.hass <joaopedrohass@outlook.com>
2023-07-16 22:43:14 -0300

    feat(dal) add CustomerContext and OrderContext

---
+++ Data/CustomerContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Customers;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CustomerContext : DbContext, IRepository<Customer>
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host=localhost;Database=customers_and_orders;Username=customers&orders_app;Password=customers");
+        }
+
+        public void Add(Customer customer) => Customers.Add(customer);
+
+        public void Delete(Customer entity) => Customers.Remove(entity);
+
+        public void Update(Customer entity) => Customers.Update(entity);
+    }
+}
---
+++ Data/OrderContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Orders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host=localhost;Database=customers_and_orders;Username=customers&orders_app;Password=customers");
+        }
+    }
+}
