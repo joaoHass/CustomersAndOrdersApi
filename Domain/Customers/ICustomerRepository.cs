99d44732dc71c32fd778e802442f26b4eeeba540
joao.hass <joaopedrohass@outlook.com>
2023-07-17 19:32:33 -0300

    feat add IcustomerRepository interface

---
+++ Domain/Customers/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+
+namespace Domain.Customers
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        ICollection<Customer> GetCustomers();
+    }
+}
