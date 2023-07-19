6ab16161ac3e2ad121bb17ee7d72187ceaf7dd5f
joao.hass <joaopedrohass@outlook.com>
2023-07-18 22:39:03 -0300

    feat add CustomerCreateDto

---
+++ Presentation/Customers/Dtos/CustomerCreateDto.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Orders;
+
+namespace Presentation.Customers.Dtos
+{
+    public record CustomerCreateDto(string Name, string CPF, string? Address, int? PhoneNumber, DateTime? BirthDate, ICollection<Order>? Orders);
+}
