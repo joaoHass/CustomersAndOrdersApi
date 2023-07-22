3169cf1a4ef9f11e86530514c6b385e08ea0af72
joao.hass <joaopedrohass@outlook.com>
2023-07-22 15:58:59 -0300

    doc documentate the Add method on ICustomerService

--- Presentation/Customers/Service/ICustomerService.cs
+++ Presentation/Customers/Service/ICustomerService.cs
@@ -6,6 +6,12 @@ namespace Presentation.Customers.Service
 {
     public interface ICustomerService 
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException">Thrown if any informed field in the DTO is invalid</exception>
         void Add(CustomerCreateDto entity);
         public void Delete(int id);
         public Customer? Get(int id);
