253d3f419cd5cf2e31e031fd9b3abbb8356bd5a2
joao.hass <joaopedrohass@outlook.com>
2023-07-22 15:38:49 -0300

    feat implement methods on ICustomerService

--- Presentation/Customers/Service/ICustomerService.cs
+++ Presentation/Customers/Service/ICustomerService.cs
@@ -1,9 +1,16 @@
 ﻿using Domain.Customers;
 using Domain.Interfaces;
+using Presentation.Customers.Dtos;
 
 namespace Presentation.Customers.Service
 {
     public interface ICustomerService 
-    { 
+    {
+        void Add(CustomerCreateDto entity);
+        public void Delete(int id);
+        public Customer? Get(int id);
+        public ICollection<Customer> GetAll();
+        public void Update(Customer entity);
+
     }
 }
