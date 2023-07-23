db13ea602dc24e08e264c37262ee1adb88f75239
joao.hass <joaopedrohass@outlook.com>
2023-07-23 16:23:06 -0300

    feat Implement GetCustomerById get method on CustomerController

--- Presentation/Customers/CustomerController.cs
+++ Presentation/Customers/CustomerController.cs
@@ -19,6 +19,20 @@ namespace Presentation
         }
 
         [HttpGet]
+        [Route("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Customer))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetCustomerById(int id)
+        {
+            Customer? customer = _service.Get(id);
+
+            if (customer == null)
+                return NotFound($"The customer with Id {id} was not found.");
+
+            return Ok(customer);
+        }
+
+        [HttpGet]
         [Route("")]
         public ActionResult<IEnumerable<Customer>> GetCustomers()
         {
