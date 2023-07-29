c143e5cb3030c740471e20afa7c81240d6df91fe
joao.hass <joaopedrohass@outlook.com>
2023-07-29 19:05:15 -0300

    chore remove unused dependencies from classes on the whole solution

--- Presentation/Security/SecurityController.cs
+++ Presentation/Security/SecurityController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Application.Authentication;
 
 namespace Presentation.Security
