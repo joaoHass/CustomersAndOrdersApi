0317e2ab266f963c05895c66f63b1c9804b8f40f
joao.hass <joaopedrohass@outlook.com>
2023-07-29 18:56:11 -0300

    feat add SecurityController

---
+++ Presentation/Security/SecurityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Authentication;
+
+namespace Presentation.Security
+{
+    public class SecurityController : Controller
+    {
+        private readonly Authentication _auth;
+
+        public SecurityController(Authentication auth)
+        {
+            _auth = auth;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("/generateToken")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult GenerateToken(string username, string password)
+        {
+            if (username == null || password == null)
+                throw new ArgumentNullException();
+
+            if (username == "John" && password == "123")
+                return Ok(_auth.GenerateToken());
+
+            return Unauthorized();
+        }
+    }
+}
